d6b4513c3c751afeb27cf487b93379a1a335986c
João Rafael Fagundes Menezes <menezes.jrafael@gmail.com>
2020-07-04 02:09:59 -0300

    Validação de Entidades

---
+++ src/DevIO.Business/Services/FornecedorService.cs
@@ -0,0 +1,39 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Business.Models.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Services
+{
+    public class FornecedorService : BaseService, IFornecedorService
+    {
+        public async Task Adicionar(Fornecedor fornecedor)
+        {
+            //Validar estado da unidade
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)
+                && !ExecutarValidacao(new EnderecoValidation(), fornecedor.Endereco)) return;
+
+            //Se nao existe fornecedor com o mesmo documento
+            return;
+        }
+
+        public async Task Atualizar(Fornecedor fornecedor)
+        {
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)) return;
+        }
+
+        public async Task AtualizarEndereco(Endereco endereco)
+        {
+            if (!ExecutarValidacao(new EnderecoValidation(), endereco)) return;
+        }
+
+        public Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
